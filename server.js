f63022f9dab2ab1535c455a42164e2a050d77dee
piyushjagtap22 <jagspiyush@gmail.com>
2023-01-15 12:41:05 +0530

    changed to use any available tcp port

--- server.js
+++ server.js
@@ -29,4 +29,5 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const server = app.listen(0, () => console.log(`Server started on port :`,server.address().port));
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
